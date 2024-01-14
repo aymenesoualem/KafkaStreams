84c293db4272ad6837032f09523478b2d9ddd774
Aymene Soualem <aymenesoualem360@gmail.com>
2024-01-14 19:30:22 +0100

    Done

--- src/main/java/org/sid/springcloudkafka/web/PageEventRestController.java
+++ src/main/java/org/sid/springcloudkafka/web/PageEventRestController.java
@@ -27,64 +27,31 @@ import java.util.Random;
 @RestController
 public class PageEventRestController {
     @Autowired
-
-    StreamBridge streamBridge;
+    private StreamBridge streamBridge;
     @Autowired
-    InteractiveQueryService interactiveQueryService;
+    private InteractiveQueryService interactiveQueryService;
     @GetMapping("/publish/{topic}/{name}")
     public PageEvent publish(@PathVariable String topic, @PathVariable String name){
-        PageEvent pageEvent = new PageEvent(name,Math.random()>0.5?"U1":"U2",new Date(),new Random().nextInt(9000));
+        PageEvent pageEvent=new PageEvent(name,Math.random()>0.5?"U1":"U2",new Date(),new Random().nextInt(9000));
         streamBridge.send(topic,pageEvent);
         return pageEvent;
+    }
+    @GetMapping(path = "/analytics",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Map<String, Long>>  analytics(){
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence->{
+                    Map<String,Long> stringLongMap=new HashMap<>();
+                    ReadOnlyWindowStore<String, Long> windowStore = interactiveQueryService.getQueryableStore("page-count", QueryableStoreTypes.windowStore());
+                    Instant now=Instant.now();
+                    Instant from=now.minusMillis(5000);
+                    KeyValueIterator<Windowed<String>, Long> fetchAll = windowStore.fetchAll(from, now);
+                    //WindowStoreIterator<Long> fetchAll = windowStore.fetch(page, from, now);
+                    while (fetchAll.hasNext()){
+                        KeyValue<Windowed<String>, Long> next = fetchAll.next();
+                        stringLongMap.put(next.key.key(),next.value);
+                    }
+                    return stringLongMap;
+                }).share();
+    }
 
-    }
-    @GetMapping(value = "/analytics",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Map<String,Long>> analytics(){
-        return Flux.interval(Duration.ofSeconds(1))
-                .map(seq->{
-                    Map<String,Long> map=new HashMap<>();
-
-                    ReadOnlyKeyValueStore<String, Long> stats = interactiveQueryService.getQueryableStore("count-store", QueryableStoreTypes.keyValueStore());
-                    Instant now=Instant.now();
-                    Instant from=now.minusSeconds(5);
-                    KeyValueIterator<String, Long> keyValueIterator = stats.all();
-                    while (keyValueIterator.hasNext()){
-                        KeyValue<String, Long> next = keyValueIterator.next();
-                        map.put(next.key,next.value);
-                    }
-                    return map;
-                });
-    }
-    @GetMapping(value = "/analyticsWindows",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Map<String,Long>> analyticsWindows(){
-        return Flux.interval(Duration.ofSeconds(1))
-                .map(seq->{
-                    Map<String,Long> map=new HashMap<>();
-                    ReadOnlyWindowStore<String, Long> stats = interactiveQueryService.getQueryableStore("count-store", QueryableStoreTypes.windowStore());
-                    Instant now=Instant.now();
-                    Instant from=now.minusSeconds(30);
-                    KeyValueIterator<Windowed<String>, Long> windowedLongKeyValueIterator = stats.fetchAll(from, now);
-                    while (windowedLongKeyValueIterator.hasNext()){
-                        KeyValue<Windowed<String>, Long> next = windowedLongKeyValueIterator.next();
-                        map.put(next.key.key(),next.value);
-                    }
-                    return map;
-                });
-    }
-    @GetMapping(value = "/analyticsAggregate",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Map<String,Double>> analyticsAggregate(){
-        return Flux.interval(Duration.ofSeconds(1))
-                .map(seq->{
-                    Map<String,Double> map=new HashMap<>();
-                    ReadOnlyWindowStore<String, Double> stats = interactiveQueryService.getQueryableStore("total-store", QueryableStoreTypes.windowStore());
-                    Instant now=Instant.now();
-                    Instant from=now.minusSeconds(30);
-                    KeyValueIterator<Windowed<String>, Double> windowedLongKeyValueIterator = stats.fetchAll(from, now);
-                    while (windowedLongKeyValueIterator.hasNext()){
-                        KeyValue<Windowed<String>, Double> next = windowedLongKeyValueIterator.next();
-                        map.put(next.key.key(),next.value);
-                    }
-                    return map;
-                });
-    }
 }
